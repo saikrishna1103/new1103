e5380753dd7a78f713faf75d69ee365a9d9eb49c
saikrishna1103 <45629193+saikrishna1103@users.noreply.github.com>
2019-08-03 10:10:52 +0530

    Update new24.py

--- new24.py
+++ new24.py
@@ -3,7 +3,7 @@ k=pow(2,s)
 w=[]
 for i in range(k):  
     m=bin(i).replace("0b","")
-    w.append(m.zfill(p))
+    w.append(m.zfill(s))
     w.sort(key=(lambda x:x.count('1')))
 for i in w:
     print(i)
