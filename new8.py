d26e8c2ac3cbf21af45ad90e8764c6dc3b9e5328
saikrishna1103 <45629193+saikrishna1103@users.noreply.github.com>
2019-08-03 09:40:15 +0530

    Update new8.py

--- new8.py
+++ new8.py
@@ -1,2 +1,11 @@
-p = int(input())
-print(p//2*(p-1) if p%2 == 0 else p//2*(p))
+import math
+s,b=map(int,input().split())
+li=list(map(int,input().split()))
+p=[]
+for i in range(0,b):
+    l,r=map(int,input().split())
+    p.append([l,r])
+for j in p:
+    e=j[0]-1
+    f=j[1]-1
+    print(math.gcd(li[e],li[f]))
